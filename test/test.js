2919146c7265a78de1cde0dcc3423394dcf1c062
Govind Singh Mehra <gmehr1@sapient.com>
2017-10-09 15:39:43 +0530

    Initial Commit

--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-var expect = require('chai').expect;
 var request = require('supertest');
 var express = require('express');
 var app = express();
@@ -6,15 +5,16 @@ var app = express();
 var uri = 'http://localhost:3000';
 
 describe('Accounts',function(){
+	const response = { Transaction: { TransactionId: 'X', PermanentAccountNumber: 'X' } };
+	
+	app.get('/accountInformation', function (req, res) {
+		res.status(200).send(response);
+    });
 
-    it('Should return user Transaction ID and Permanent Account Number',function(done){
-        app.get(uri + '/accountinformation/?AccountNumber='+100+'&SortCode='+49).end(function(err,res){
-
-                var body = res.body;
-
-                expect(data.Transactions.TransactionId).to.equal('X');
-                expect(data.Transactions.PermanentAccountNumber).to.equal('X');
-                
-            });
+    it('Should return user Transaction ID and Permanent Account Number', function(done) {
+		request(app)
+		.get('/accountInformation')
+		.expect(200, response, done);
     });
 });
+
